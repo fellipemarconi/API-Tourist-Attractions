8d0d8f11e92216bc66e1e05814229e103663435e
fellipemarconi <fellipeluan.marconi@gmail.com>
2024-02-08 19:43:02 -0300

    Created Model TouristSpot and registed in Admin area with begin fields

--- apps/tourist_attractions/admin.py
+++ apps/tourist_attractions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.TouristSpot)
+class TouristSpotAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_approved')
+    search_fields = ('id', 'name')
+    ordering = '-id',
+    list_per_page = 25
+    list_max_show_all = 100
--- apps/tourist_attractions/models.py
+++ apps/tourist_attractions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class TouristSpot(models.Model):
+    class Meta:
+        verbose_name = 'Tourist Spot'
+        verbose_name_plural = 'Tourist Spots'
+        
+    name = models.CharField(max_length=150)
+    description = models.TextField(max_length=4000)
+    is_approved = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
