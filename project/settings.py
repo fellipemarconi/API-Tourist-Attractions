11beac258b3654b2e723aee5b968c01eace45b68
Fellipe <anksrr@proton.me>
2024-02-21 00:05:03 -0300

    (raw): Deleted a staticfiles_dir

--- project/settings.py
+++ project/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 STATIC_ROOT = BASE_DIR / 'static_collected'
 
 MEDIA_URL = 'media/'
