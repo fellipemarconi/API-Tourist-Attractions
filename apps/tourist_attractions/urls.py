bac0794280bc655be5134b76a6761915793b9157
Fellipe <anksrr@proton.me>
2024-02-21 00:03:42 -0300

    (raw): Added a media url to settings

--- apps/tourist_attractions/urls.py
+++ apps/tourist_attractions/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-
-from .api import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('apps.tourist_attractions.api.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
