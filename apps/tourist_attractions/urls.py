aa7171bc23fdafae1d24a0b5a07d03d51ecbdeda
Fellipe <anksrr@proton.me>
2024-02-21 00:10:21 -0300

    (raw): Changed url to media files to project/urls.py

--- apps/tourist_attractions/urls.py
+++ apps/tourist_attractions/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', include('apps.tourist_attractions.api.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
