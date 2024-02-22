1f83ff40324db10898a64b7cd176d8efc930dde2
Fellipe <anksrr@proton.me>
2024-02-22 15:36:23 -0300

    (feat): Added permission class IsAuthenticatedOrReadOnly and Token authentication

--- apps/tourist_attractions/api/views.py
+++ apps/tourist_attractions/api/views.py
@@ -2,11 +2,15 @@ from rest_framework.viewsets import ModelViewSet
 from ..models import TouristSpot
 from .serializers import TouristSpotSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.authentication import TokenAuthentication
 
 class TouristAttractionViewSet(ModelViewSet):
     serializer_class = TouristSpotSerializer
     filter_backends = [DjangoFilterBackend, ]
     filterset_fields = ['name', 'description', 'attractions']
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+    authentication_classes = [TokenAuthentication, ]
     
     def get_queryset(self):
         queryset = TouristSpot.objects.filter(is_approved=True)
